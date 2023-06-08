8a3fd465cc50a91fb54ddea2f8425760d9a65326
LasyaVeldurti <lasyasreeveldurti18@gmail.com>
2023-06-08 17:11:02 +0000

    emoji

--- src/components/EmojiGame/index.js
+++ src/components/EmojiGame/index.js
@@ -20,40 +20,129 @@ import './index.css'
 
 // Write your code here.
 class EmojiGame extends Component {
-  state = {clickedEmoji: [], isClicked: false}
+  state = {
+    clickedEmoji: [],
+    isClicked: false,
+    score: 0,
+    topScore: 0,
+    gameOver: false,
+    status: '',
+  }
 
   onClickEmoji = id => {
+    console.log(id)
+    let {emojisList} = this.props
+    const {isClicked, score} = this.state
+    const {length} = emojisList.length
     const {clickedEmoji} = this.state
-    const shuffledEmojisList = () => {
-      const {emojisList} = this.propss
-      return emojisList.sort(() => Math.random() - 0.5)
-    }
-    console.log(shuffledEmojisList)
-    if (clickedEmoji.includes(id)) {
-      this.setState(prevState => ({isClicked: !prevState.isClicked}))
-    } else {
+
+    if (clickedEmoji.includes(id) === false) {
       this.setState(prevState => ({
         clickedEmoji: [...prevState.clickedEmoji, id],
+        score: prevState.score + 1,
       }))
-      console.log(clickedEmoji)
     }
+
+    const shuffledEmojisList = emojisList.sort(() => Math.random() - 0.5)
+    emojisList = shuffledEmojisList
+
+    if (clickedEmoji.includes(id)) {
+      this.setState(prevState => ({isClicked: !prevState.isClicked}))
+    }
+    console.log(clickedEmoji)
+
+    if (isClicked) {
+      this.setState(prevState => ({gameOver: !prevState.gameOver}))
+      this.setState({status: 'Lose'})
+    }
+    if (score === length) {
+      this.setState(prevState => ({gameOver: !prevState.gameOver}))
+      this.setState({status: 'Win'})
+    }
+  }
+
+  onClickPlayAgain = () => {
+    const {score, topScore} = this.state
+    if (topScore < score) {
+      this.setState({topScore: score})
+    }
+    this.setState({score: 0})
+    this.setState(prevState => ({gameOver: !prevState.gameOver}))
   }
 
   render() {
     const {emojisList} = this.props
+    const {gameOver, status, score, topScore} = this.state
+    let landingPage
+    if (status === 'win') {
+      landingPage = (
+        <div className="landing-page-container">
+          <div className="text-container">
+            <h1>You Won</h1>
+            <p>Best Score</p>
+            <p>
+              {score}/{emojisList.length}
+            </p>
+            <button
+              onClick={this.onClickPlayAgain}
+              className="play-again-btn"
+              type="button"
+            >
+              Play Again
+            </button>
+          </div>
+          <img
+            className="landing-img"
+            src="https://assets.ccbp.in/frontend/react-js/won-game-img.png"
+            alt="win"
+          />
+        </div>
+      )
+    } else {
+      landingPage = (
+        <div className="landing-page-container">
+          <div className="text-container">
+            <h1>You Lose</h1>
+            <p> Score</p>
+            <p>
+              {' '}
+              {score}/{emojisList.length}{' '}
+            </p>
+            <button
+              onClick={this.onClickPlayAgain}
+              className="play-again-btn"
+              type="button"
+            >
+              Play Again
+            </button>
+          </div>
+          <img
+            className="landing-img"
+            src="https://assets.ccbp.in/frontend/react-js/lose-game-img.png"
+            alt="lose"
+          />
+        </div>
+      )
+    }
 
     return (
       <div className="bg-container">
-        <NavBar />
-        <ul className="emoji-ul-container">
-          {emojisList.map(eachEmoji => (
-            <EmojiCard
-              emojiDetails={eachEmoji}
-              key={eachEmoji.id}
-              clickEmoji={this.onClickEmoji}
-            />
-          ))}
-        </ul>
+        <NavBar score={score} topScore={topScore} />
+        {gameOver ? (
+          <>{landingPage}</>
+        ) : (
+          <>
+            <ul className="emoji-ul-container">
+              {emojisList.map(eachEmoji => (
+                <EmojiCard
+                  emojiDetails={eachEmoji}
+                  key={eachEmoji.id}
+                  clickEmoji={this.onClickEmoji}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     )
   }
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,15 +1,23 @@
 // Write your code here.
 import './index.css'
 
-const NavBar = () => (
-  <div className="navbar-container">
-    <img
-      className="logo-img"
-      src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
-      alt="emoji logo"
-    />
-    <p className="title">Emoji Game</p>
-  </div>
-)
-
+const NavBar = props => {
+  const {score, topScore} = props
+  return (
+    <div className="navbar-container">
+      <div className="logo-container">
+        <img
+          className="logo-img"
+          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+          alt="emoji logo"
+        />
+        <h1 className="title">Emoji Game</h1>
+      </div>
+      <div className="score-container">
+        <p className="title">Score: {score} </p>
+        <p className="title">Top Score : {topScore} </p>
+      </div>
+    </div>
+  )
+}
 export default NavBar
